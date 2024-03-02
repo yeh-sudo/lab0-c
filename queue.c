9d4f06447dcb6e7376675157d81a189f8f123726
yeh-sudo <yehchanshen@gmail.com>
2024-03-02 15:27:12 +0800

    Implement q_reverse function
    Implement q_reverse function but not test yet. It is combined with
    other functions in test case. Not sure if it is right or wrong.

--- queue.c
+++ queue.c
@@ -186,7 +186,18 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return;
+    }
+    struct list_head *node;
+    for (node = head; node != head; node = node->prev) {
+        struct list_head *next = node->next;
+        node->next = node->prev;
+        node->prev = next;
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
